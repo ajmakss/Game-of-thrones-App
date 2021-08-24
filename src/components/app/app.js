5c31c198b0735c3dd6b2515f3256799907623143
Maksym Bychkiv <maxim.bichkiv@gmail.com>
2021-08-24 18:10:45 +0300

    hidding Random Character

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,31 +4,59 @@ import Header from '../header';
 import RandomChar from '../randomChar';
 import ItemList from '../itemList';
 import CharDetails from '../charDetails';
+import styled from 'styled-components';
 
+const ToggleRandomButton = styled.button`
+        width: 319px;
+        height: 40px;
+        margin-bottom: 20px;
+        background: rgb(4,17,68);
+        border: none;
+        color: white;
+        &:focus{
+            border: none;
+        }
+`
 
-const App = () => {
-    return (
-        <> 
-            <Container>
-                <Header />
-            </Container>
-            <Container>
-                <Row>
-                    <Col lg={{size: 5, offset: 0}}>
-                        <RandomChar/>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col md='6'>
-                        <ItemList />
-                    </Col>
-                    <Col md='6'>
-                        <CharDetails />
-                    </Col>
-                </Row>
-            </Container>
-        </>
-    );
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            randomChar: true
+        }
+        this.hiddenRandomChar = this.hiddenRandomChar.bind(this);
+    }
+    hiddenRandomChar(){
+        let status = this.state.randomChar;
+       this.setState({
+           randomChar: !status
+       })
+    }
+    render() {
+        const {randomChar} = this.state;
+        const randomCharacter = randomChar ?  <RandomChar/> : null;
+        return (
+            <> 
+                <Container>
+                    <Header />
+                </Container>
+                <Container>
+                    <Row>
+                        <Col lg={{size: 5, offset: 0}}>
+                             {randomCharacter}
+                            <ToggleRandomButton onClick={this.hiddenRandomChar}> Toggle random character</ToggleRandomButton>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col md='6'>
+                            <ItemList />
+                        </Col>
+                        <Col md='6'>
+                            <CharDetails />
+                        </Col>
+                    </Row>
+                </Container>
+            </>
+        );
+    }
 };
-
-export default App;
